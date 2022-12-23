c93fec7273b505270fb0b1ba620f6b134b85a2e9
Nika <nikarasevas@gmail.com>
2022-12-23 11:41:39 +0300

    done

--- commands.py
+++ commands.py
@@ -8,10 +8,10 @@ async def start_bot(message: types.Message):
     img = open('konfetki.jpeg', 'rb')
     await bot.send_photo(message.chat.id, img)
     await bot.send_message(message.from_user.id, text=f'{message.from_user.first_name}, '
-                                                      f'приветствую тебя в игре Ника и Конфетки! '
+                                                      f'приветствую тебя в игре Никины Конфетки! '
                                                       f'Вызвав команду /help ты можешь ознакомиться с правилами игры.\n'
-                                                      f'Если ты готов – нажми команду \n'
-                                                      f'/lottery, чтобы кинуть жребий.')
+                                                      f'Если ты готов – выбирай уровень сложности.\n'
+                                                      f'Набери команду /N, если ты новичок и /S – если супергерой в конфетках.')
 
 
 
@@ -24,6 +24,28 @@ async def help_bot(message: types.Message):
                                                       f'Выигрывает тот, кто последний заберёт оставшиеся на столе конфеты.\n'
                                                       f'Если ты готов – нажимай /start. Да начнётся битва!')
 
+
+total_sweets = 150
+player = ''
+
+@dp.message_handler(commands=['N'])
+async def level_N(message: types.Message):
+    global player
+    img = open('Nov.jpeg', 'rb')
+    await bot.send_photo(message.chat.id, img)
+    player = 'Nov'
+    await message.reply(f'Включаю твою версию для новичка!\n'
+                        f'Нажми команду /lottery, чтобы кинуть жребий.')
+
+
+@dp.message_handler(commands=['S'])
+async def level_S(message: types.Message):
+    global player
+    img = open('Sup.jpeg', 'rb')
+    await bot.send_photo(message.chat.id, img)
+    player = 'Sup'
+    await message.reply(f'Да ты уверен в себе! Включаю твою супергеройский левел!\n'
+                        f'Нажми команду /lottery, чтобы кинуть жребий.')
 
 @dp.message_handler(commands=['lottery'])
 async def first_move(message: types.Message):
@@ -41,11 +63,13 @@ async def lottery(message: types.Message):
         await bot_turn(message)
 
 
-total_sweets = 150
-
 async def bot_turn(message: types.Message):
     global total_sweets
-    take_sweets_bot = total_sweets % 29 if total_sweets % 29 != 0 else rnd(1, 28)
+    global player
+    if player == 'Nov':
+        take_sweets_bot = rnd(1, 28)
+    else:
+        take_sweets_bot = total_sweets % 29 if total_sweets % 29 != 0 else rnd(1, 28)
     total_sweets -= take_sweets_bot
     await bot.send_message(message.from_user.id, text=f'Бот взял со стола {take_sweets_bot} конфет. На столе осталось {total_sweets} конфет.')
     if total_sweets > 0:
@@ -68,16 +92,11 @@ async def player_turn(message: types.Message):
             if total_sweets > 0:
                 await bot_turn(message)
             else:
-                await bot.send_message(message.from_user.id, text=f'Ух ты, ты обыграл искуственный интеллект, поздравляю!')
+                await bot.send_message(message.from_user.id, text=f'Да ты конфетный супермен! Поздравляю с выигрышем!')
                 total_sweets = 150
                 await bot.send_message(message.from_user.id, text=f'Сыграем заново?) Жмакай /start!')
         else:
             await bot.send_message(message.from_user.id, text=f'Возьми-ка поменьше конфет!')
     else:
         await bot.send_message(message.from_user.id, text=f'Эй, нужно ввести число!')
-
-
-
-
-
 
